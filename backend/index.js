c891167defa564d6c5544b6043e52bd105314acc
sariandras43 <sariandras43@gmail.com>
2025-03-22 20:28:45 +0100

    Filestreaming works on backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 require('dotenv').config();
 const app = express();
 
-const path = process.env.VIDEOS_PATH
+const path = process.env.VIDEOS_PATH;
+const port = process.env.port;
 
 app.get("/videos", async (req, res) => {
     await fs.readdir(path, (err, files) => {
@@ -20,16 +21,24 @@ app.get("/videos", async (req, res) => {
 app.get("/video/:file", (req, res) => {
     const range = req.headers.range;
     const file = req.params.file;
+    const acceptedExtensions = ["mp4","mkv"]
     if (!range) {
-        res.status(400).send("Range headere kötelező");
+        res.status(400).send("Range header kötelező");
         return;
     }
-    if (!file || file.split('.').length < 2 || file.split('.')[1] != "mp4") {
+    let fileNameSplit;
+    if (!file) {
         res.status(400).json({ message: "Érvénytelen fájlnév." });
         return;
+    }else{
+        fileNameSplit = file.split('.');
+        if (fileNameSplit.length<2 || !acceptedExtensions.includes(fileNameSplit[fileNameSplit.length-1])) {
+            res.status(400).json({ message: "Érvénytelen kiterjesztés." });  
+            return;   
+        }
     }
-    const videoPath = "sample.mkv";
-    const videoSize = fs.statSync("sample.mkv").size;
+    const videoPath = path + '/' + file;
+    const videoSize = fs.statSync(videoPath).size;
     const CHUNK_SIZE = 10 ** 6;
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
@@ -38,7 +47,7 @@ app.get("/video/:file", (req, res) => {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
         "Accept-Ranges": "bytes",
         "Content-Length": contentLength,
-        "Content-Type": "video/mkv",
+        "Content-Type": `video/${fileNameSplit[fileNameSplit.length-1]}`,
     };
     res.writeHead(206, headers);
     const videoStream = fs.createReadStream(videoPath, { start, end });
@@ -46,6 +55,6 @@ app.get("/video/:file", (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}!`);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
 });
